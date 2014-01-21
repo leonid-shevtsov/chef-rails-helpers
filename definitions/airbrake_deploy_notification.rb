d61a5817399f065e6d1490f91bb89fc8a5ce4d56
Leonid Shevtsov <leonid@shevtsov.me>
2014-01-21 16:52:35 +0200

    airbrake deploy notification

---
+++ definitions/airbrake_deploy_notification.rb
@@ -0,0 +1,18 @@
+define :airbrake_deploy_notification, {
+  :name => nil,
+  :path => nil,
+  :environment => nil,
+  :key => nil,
+  :host => nil,
+  } do
+
+    name = params[:name]
+    path = params[:path] || "/home/#{name}/apps/#{name}/current"
+    environment = params[:environment] || 'production'
+    key = params[:key] || node[:airbrake][name][:key]
+    host = params[:host] || node[:airbrake][name][:host] || 'airbrake.io'
+
+    bash "airbrake deploy notification for #{name}" do
+      code %Q{cd #{path} && curl -d "api_key=#{key}&deploy[rails_env]=#{environment}&deploy[scm_revision]=`git rev-parse HEAD`&deploy[scm_repository]=`git config --get remote.origin.url`" http://#{host}/deploys}
+    end
+end
