93982e9a54e71503e1413915645c9c5e7da58daa
Leonid Shevtsov <leonid@shevtsov.me>
2015-03-28 10:21:06 +0200

    Added delete action to Unicorn recipe

--- definitions/unicorn.rb
+++ definitions/unicorn.rb
@@ -23,7 +23,8 @@ define :unicorn, {
   :environment => 'production',
   :worker_processes => 2,
   :rvm => false,
-  :monit => true
+  :monit => true,
+  :action => :create
   } do
 
   name = params[:name]
@@ -32,49 +33,66 @@ define :unicorn, {
   the_owner = params[:owner] || name
   the_group = params[:group] || name
 
-  template "#{config_path}/unicorn.rb" do
-    owner the_owner
-    group the_group
-    source 'unicorn.rb.erb'
-    variables({
-      :app_name => name,
-      :app_path => app_path,
-      :owner => the_owner,
-      :group => the_group,
-      :worker_processes => params[:worker_processes]
-    })
-    cookbook 'rails_helpers'
-  end
-
-  template "/etc/init.d/#{name}-unicorn" do
-    owner 'root'
-    group 'root'
-    mode 0755
-    source "unicorn.initd.erb"
-    variables({
-      :app_name => name,
-      :app_path => app_path,
-      :owner => the_owner,
-      :rvm => params[:rvm],
-      :environment => params[:environment]
-    })
-    cookbook 'rails_helpers'
-  end
-
-  service "#{name}-unicorn" do
-    status_command "/etc/init.d/#{name}-unicorn status"
-    supports status: true, restart: true
-    action :enable
-  end
-
-  if params[:monit]
-    template "/etc/monit/conf.d/#{name}-unicorn.conf" do
-      owner 'root'
-      group 'root'
-      mode 0700
-      source "unicorn.monit.erb"
-      variables :app_name => name
+  if params[:action] == :create
+    template "#{config_path}/unicorn.rb" do
+      owner the_owner
+      group the_group
+      source 'unicorn.rb.erb'
+      variables({
+        :app_name => name,
+        :app_path => app_path,
+        :owner => the_owner,
+        :group => the_group,
+        :worker_processes => params[:worker_processes]
+      })
       cookbook 'rails_helpers'
     end
+
+    template "/etc/init.d/#{name}-unicorn" do
+      owner 'root'
+      group 'root'
+      mode 0755
+      source "unicorn.initd.erb"
+      variables({
+        :app_name => name,
+        :app_path => app_path,
+        :owner => the_owner,
+        :rvm => params[:rvm],
+        :environment => params[:environment]
+      })
+      cookbook 'rails_helpers'
+    end
+
+    service "#{name}-unicorn" do
+      status_command "/etc/init.d/#{name}-unicorn status"
+      supports status: true, restart: true
+      action :enable
+    end
+
+    if params[:monit]
+      template "/etc/monit/conf.d/#{name}-unicorn.conf" do
+        owner 'root'
+        group 'root'
+        mode 0700
+        source "unicorn.monit.erb"
+        variables :app_name => name
+        cookbook 'rails_helpers'
+      end
+    end
+  elsif params[:action] == :remove
+    file "/etc/monit/conf.d/#{name}-unicorn.conf" do
+      action :delete
+    end
+    service "#{name}-unicorn" do
+      action [:stop, :disable]
+    end
+    file "/etc/init.d/#{name}-unicorn" do
+      action :delete
+    end
+    file "#{config_path}/unicorn.rb" do
+      action :delete
+    end
+  else
+    raise 'Unknown action'
   end
 end
